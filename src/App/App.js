49206b7c7d9805ee083de4035ef914da4b8e4471
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-03 13:16:55 +0200

    added Submit and Reset buttons

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import styles from './App.module.css'
 
 function App() {
     const [buttons, setButtons] = useState([]);
-    const [resultNumb, setResultNumber] = useState(10);
+    const [resultNumb, setResultNumber] = useState(0);
     const [range, setRange] = useState(
         {
             lower: 0,
@@ -12,37 +12,51 @@ function App() {
     );
 
     const onChange = (e) => {
-        setRange({...range, [e.target.name]: e.target.value});
+        setRange({...range, [e.target.name]: +e.target.value});
+        console.log('range  :', range);
+
     };
+    const onButtonClick = (e) => {
+        setResultNumber(resultNumb + +e);
+
+        console.log('setResultNumber :', setResultNumber);
+
+    };
+
 
     const onSubmitRange = (e) => {
         e.preventDefault();
         const minRange = range["lower"];
         const maxRange = range["upper"];
+        console.log('minRange: ',minRange );
+        console.log('maxRange: ',maxRange );
+
         if (minRange < maxRange) {
             let arr = [];
             for (let i = minRange; i <= maxRange; i++) {
                 arr.push(i);
             }
-
             setButtons(arr);
+            console.log('setButtons(arr) : ',arr);
+
         }
     };
 
-    const onButtonClick = (e) => {
-        setResultNumber(resultNumb + +e);
+    const onReset = () => {
+        setResultNumber(0)
     };
+
 
     return (
         <main className={styles.app}>
             <div className={styles.container}>
                 <h1> Enter your range from 1 to 5 </h1>
-                <form className={styles.counter} >
+                <form className={styles.counter}  onSubmit={onSubmitRange}>
                     <div className={styles.inputs}>
                         <input type="text" name="lower" onChange={onChange}/>
                         <input type="text" name="upper" onChange={onChange}/>
                     </div>
-                    <button  key={123} onClick={onSubmitRange}> </button>
+                    <button  key={123} onClick={onSubmitRange} className={styles.submit}> SUBMIT </button>
                     <div className={styles.buttons}>
                         <div className={styles.negatives}>
                             {buttons.reverse().map(el => <button key={-el}
@@ -55,9 +69,10 @@ function App() {
                             {buttons.reverse().map(el => <button key={el}
                                                                  onClick={() => onButtonClick(el)}
                                                                  className={styles.button}>
-                                {el}
+                                +{el}
                             </button>)}</div>
                     </div>
+                    <button  key={345} onClick={onReset} className={styles.reset}> RESET </button>
 
                 </form>
             </div>
