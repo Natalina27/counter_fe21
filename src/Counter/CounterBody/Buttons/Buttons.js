faa03438f957edc1b228486a3723df34adbe51c4
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-13 23:00:35 +0200

    removed reverse() from button component

--- src/Counter/CounterBody/Buttons/Buttons.js
+++ src/Counter/CounterBody/Buttons/Buttons.js
@@ -8,14 +8,14 @@ function Buttons(
     return (
         <div className={styles.buttons}>
             <div className={styles.negatives}>
-                {negativeRange.reverse().map(el => <button key={-el}
+                {negativeRange.map(el => <button key={-el}
                                                      onClick={() => onButtonClick(-el)}
                                                      className={styles.button}>
                     {-el}
                 </button>)}</div>
             <span className={styles.number}>{resultNumb}</span>
             <div className={styles.positives}>
-                {positiveRange.reverse().map(el => <button key={el}
+                {positiveRange.map(el => <button key={el}
                                                      onClick={() => onButtonClick(el)}
                                                      className={styles.button}>
                     +{el}
